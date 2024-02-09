8719b2e2328890808b37cf32170dfa8275dd2f51
Adarsh yadav <92322679+Meadarsh@users.noreply.github.com>
2024-02-09 11:38:10 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import React from "react"
+import React, { useEffect } from "react"
 import LandingPage from "./pages/landingPage"
 import SecondPage from "./pages/secondPage"
 import ThirdPage from "./pages/thirdPage"
 import FourthPage from "./pages/fourthPage"
 import Footer from "./components/footer"
+import DesktopOnly from "./components/desktopOnly"
 function App() {
-
+  const[width, setWidth] = React.useState(window.innerWidth)
+useEffect(()=>{
+  setWidth(window.innerWidth)
+},[])
   return (
-    <>
+   <>
+    {width>768?<>
 <LandingPage/>
 <SecondPage/>
 <ThirdPage/>
 <FourthPage/>
 <Footer/>
+  </>:<DesktopOnly/>}
   </>
   )
 }
---
+++ src/components/desktopOnly.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DesktopOnly = () => {
+  return (
+    <div className='bg-black text-white h-[100vh] w-[100vw] flex items-center justify-center' >
+        <h1>
+         This site is currently for desktop only
+        </h1>
+    </div>
+  )
+}
+
+export default DesktopOnly
